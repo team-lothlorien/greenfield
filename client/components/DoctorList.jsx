a31ab3bd774b3f57921919cd45319ce228cf9773
Eric Meltzer <etmeltzer@gmail.com>
2018-07-17 18:23:17 -0400

    More work on doctor components

--- client/components/DoctorList.jsx
+++ client/components/DoctorList.jsx
@@ -1,6 +1,8 @@
+import DoctorListEntry from './DoctorListEntry.jsx';
+import React from 'react';
 var DoctorList = (props) => {
   var DoctorListEntries = props.doctors.map((doctor) => {
-    return <DoctorListEntry doctor={doctor} />;
+    return <DoctorListEntry doctor={doctor} key={doctor.uid}/>;
   });
   return(
     <div>{DoctorListEntries}</div>
