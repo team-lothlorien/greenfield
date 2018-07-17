8bf96e5a735c2f8e764178025ec24af4206264d5
David Levy <lennox.davidlevy@gmail.com>
2018-07-17 13:07:22 -0400

    autocomplete working but ugly

--- client/components/Search.jsx
+++ client/components/Search.jsx
@@ -1,17 +1,83 @@
 import React from 'react';
+import axios from 'Axios';
+import Autosuggest from 'react-autosuggest';
 
 class Search extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       term: '',
-      dropDown: 'Keyword'
+      dropDown: 'Keyword',
+      conditions: [],
+      value: '',
+      suggestions: []
     };
     this.onFilterChange = this.onFilterChange.bind(this);
     this.onTermChange = this.onTermChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    //AUTOSUGGEST
+    this.onSuggestChange = this.onSuggestChange.bind(this);
+    this.getConditions = this.getConditions.bind(this);
+    this.onSuggestionsFetchRequested = this.onSuggestionsFetchRequested.bind(this);
+    this.getSuggestionValue = this.getSuggestionValue.bind(this);
+    this.renderSuggestion = this.renderSuggestion.bind(this);
+    this.escapeRegexCharacters = this.escapeRegexCharacters.bind(this);
+    this.getSuggestions = this.getSuggestions.bind(this);
+    this.onSuggestionsClearRequested = this.onSuggestionsClearRequested.bind(this);
 
   }
+  componentDidMount() {
+    this.getConditions();
+    // this.onSuggestionsClearRequested();
+  }
+
+  getConditions() {
+    axios.get('https://api.betterdoctor.com/2016-03-01/conditions?user_key=f695212b8cce3cacd996361881ce040b')
+    .then((condition) => {
+      console.log(condition.data);
+      this.setState({conditions: condition.data.data});
+      // .catch(err => console.log(err));
+    });
+  }
+
+  //***********AUTOSUGGEST**********************//
+
+  escapeRegexCharacters(str) {
+    return str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+  }
+  getSuggestions(value) {
+    // const conditions = this.state.conditions
+    const escapedValue = this.escapeRegexCharacters(value.trim());
+    if (escapedValue === '') {
+      return [];
+    }
+    const regex = new RegExp('^' + escapedValue, 'i');
+    return this.state.conditions.filter(condition => regex.test(condition.name));
+  }
+  getSuggestionValue(suggestion) {
+    return suggestion.name;
+  }
+  renderSuggestion(suggestion) {
+    return (
+      <span>{suggestion.name}</span>
+    );
+  }
+  onSuggestChange (event, { newValue, method }) {
+    this.setState({
+      value: newValue
+    });
+  }
+  onSuggestionsFetchRequested({ value }) {
+    this.setState({
+      suggestions: this.getSuggestions(value)
+    });
+  }
+  onSuggestionsClearRequested() {
+    this.setState({
+      suggestions: []
+    });
+  }
+
 
   onFilterChange(event) {
     this.setState({dropDown: event.target.value});
@@ -25,6 +91,13 @@ class Search extends React.Component {
   }
 
   render() {
+    const { value, suggestions } = this.state;
+    const inputProps = {
+      placeholder: 'Soft Skull Portion',
+      value,
+      onChange: this.onSuggestChange
+    };
+
     //temp data until we have an object from
     let filterOptions = ['Keyword', 'Symptoms', 'Location', 'Doctors', 'Specialties', 'Language'];
     let filterList = filterOptions.map(item => {
@@ -40,6 +113,13 @@ class Search extends React.Component {
             {filterList}
           </select>
         </label>
+        <Autosuggest
+          suggestions={suggestions}
+          onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+          onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+          getSuggestionValue={this.getSuggestionValue}
+          renderSuggestion={this.renderSuggestion}
+          inputProps={inputProps} />
         <input type="text" value={this.state.term} onChange={this.onTermChange}/>
         <input type="submit" value="Search" />
       </form>
