9c1c33e39e32f14aeefdbadbe54d2211f334cf9b
Peter Park <parkdhp@gmail.com>
2018-07-18 11:24:41 -0400

    Server succesfully fetches doctors, conditions, specialties, and insurances

--- server/apiHelpers/findSpecialties.js
+++ server/apiHelpers/findSpecialties.js
@@ -4,6 +4,6 @@ const config = require('../config');
 module.exports.findSpecialties = function () {
   let url = `https://api.betterdoctor.com/2016-03-01/specialties?user_key=${config.API_KEY}`
   return axios.get(url)
-  .then(data => data.data.map(specialty => specialty.name))
+  .then(data => data.data.data.map(specialty => specialty.name))
   .catch(err => console.error(err))
 }
--- server/controllers/insuranceController.js
+++ server/controllers/insuranceController.js
@@ -1,8 +1,10 @@
-const apiHelper = require('../apiHelpers/findDoctors');
+const apiHelper = require('../apiHelpers/findInsurances');
 
 module.exports = {
   //returns search result of doctors array
   get: (req, res) => {
     apiHelper.findInsurances()
+    .then(insurances => res.status(200).send(insurances))
+    .catch(err => console.error('Error fetching insurances: ', err))
   }
 }
