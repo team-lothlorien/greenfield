270714dbdee8f6e8ecc838557219fd4094ed623f
Peter Park <parkdhp@gmail.com>
2018-07-19 17:10:49 -0400

    temp

---
+++ client/components/CommentBox.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+var CommentBox = (props) => {
+  return(
+    <div className="CommentBoxCard">
+      {/* Comment Box Text Input Here */}
+      {/* Comment Box Submit Button Here */}
+      {/* List of Comments for this Doctor Here */}
+      {/* If currUser = username, conditional render of delete comment */}
+    </div>
+  );
+};
+
+export default CommentBox;
