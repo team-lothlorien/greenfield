3217af16a76297bdbca883a758558184ab2c627b
Peter Park <parkdhp@gmail.com>
2018-07-19 13:58:35 -0400

    [completed] comments and favorites db and server done

--- server/routes/comments.js
+++ server/routes/comments.js
@@ -3,7 +3,7 @@ const commentsRouter = require('express').Router();
 
 commentsRouter.get('/', commentsController.get);
 commentsRouter.post('/', commentsController.post);
-commentsRouter.put('/', commentsController.put);
+// commentsRouter.put('/', commentsController.put);
 commentsRouter.delete('/', commentsController.delete);
 
 module.exports = commentsRouter;
