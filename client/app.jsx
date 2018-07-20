4aae6e758ef30ea865e4d7ac1e88772741a9a1e7
David Levy <lennox.davidlevy@gmail.com>
2018-07-20 09:23:58 -0400

    whatever changes i dunno

--- client/app.jsx
+++ client/app.jsx
@@ -21,7 +21,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      user: '',
+      user: 'Guest',
       doctors: [],
       compare: [],
       location: '',
